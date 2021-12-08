96119795242d6f89ea55e8db28010be760ca9482
MichaelHulme <coachhulmeumb@gmail.com>
2021-12-08 14:38:26 -0500

    added new script

--- server/index.js
+++ server/index.js
@@ -22,6 +22,7 @@ mongoose.connect(
     }
 )
 
+app.use(express.static(path.resolve(__dirname, "./frontend/build")))
 app.use(express.static('public'));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
