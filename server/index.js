87bbfbb9e4f1fbf66d2b85f251733b8a80c83898
MichaelHulme <coachhulmeumb@gmail.com>
2021-12-08 15:12:56 -0500

    removed heroku-postbuild

--- server/index.js
+++ server/index.js
@@ -22,7 +22,6 @@ mongoose.connect(
     }
 )
 
-app.use(express.static(path.resolve(__dirname, "./frontend/build")))
 app.use(express.static('public'));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
